ad81ff5f8813f27503564f82445e196ed6dce072
jlew <jlew.blackout@gmail.com>
2011-01-22 07:03:32 -0500

    Added private config option so we can push to git safely

--- models/db.py
+++ models/db.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*- 
 
-#########################################################################
-## This scaffolding model makes your app work on Google App Engine too
-#########################################################################
-
 DATE_FORMAT = "%m/%d/%y %H:%M:%S %p"
 
+#########################################################################
+## Google App Engine Support
+#########################################################################
 if request.env.web2py_runtime_gae:            # if running on Google App Engine
     db = DAL('gae')                           # connect to Google BigTable
     session.connect(request, response, db = db) # and store sessions and tickets there
@@ -15,19 +14,8 @@ if request.env.web2py_runtime_gae:            # if running on Google App Engine
     # session.connect(request, response, db = MEMDB(Client()))
 else:                                         # else use a normal relational database
     db = DAL('sqlite://storage.sqlite')       # if not, use SQLite or other DB
-## if no need for session
-# session.forget()
 
-#########################################################################
-## Here is sample code if you need for 
-## - email capabilities
-## - authentication (registration, login, logout, ... )
-## - authorization (role based authorization)
-## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
-## - crud actions
-## (more options discussed in gluon/tools.py)
-#########################################################################
-
+import os
 from gluon.tools import *
 mail = Mail()                                  # mailer
 auth = Auth(globals(),db)                      # authentication/authorization
@@ -35,11 +23,28 @@ crud = Crud(globals(),db)                      # for CRUD helpers using auth
 service = Service(globals())                   # for json, xml, jsonrpc, xmlrpc, amfrpc
 plugins = PluginManager()
 
-mail.settings.server = 'logging' or 'smtp.gmail.com:587'  # your SMTP server
-mail.settings.sender = 'you@gmail.com'         # your email
-mail.settings.login = 'username:password'      # your credentials or None
 
-auth.settings.hmac_key = 'sha512:0a715450-99df-4dc2-a70e-0bd3f458dd1d'   # before define_tables()
+#########################################################################
+## Private Auth settings should be read for private_cfg.ini if exists
+#########################################################################
+private_cfg = os.path.join(request.folder,"models","private_cfg.ini")
+if os.path.exists( private_cfg ):
+    import ConfigParser
+    config = ConfigParser.ConfigParser()
+    config.read(private_cfg)
+    mail.settings.server = config.get('Mail', 'server')
+    mail.settings.tls = config.get('Mail', 'tls')
+    mail.settings.sender = config.get('Mail', 'sender')
+    mail.settings.login = config.get('Mail', 'login')
+    
+    auth.settings.hmac_key = config.get('Auth', 'hmac_key')
+    
+else:
+    mail.settings.server = 'logging' or 'smtp.gmail.com:587'  # your SMTP server
+    mail.settings.sender = 'you@gmail.com'         # your email
+    mail.settings.login = 'username:password'      # your credentials or None
+
+    auth.settings.hmac_key = 'sha512:0a715450-99df-4dc2-a70e-0bd3f458dd1d'   # before define_tables()
 
 auth.settings.create_user_groups = False
 auth.settings.actions_disabled.append('register') 
@@ -52,33 +57,11 @@ auth.messages.verify_email = 'Click on the link http://'+request.env.http_host+U
 auth.settings.reset_password_requires_verification = True
 auth.messages.reset_password = 'Click on the link http://'+request.env.http_host+URL(r=request,c='default',f='user',args=['reset_password'])+'/%(key)s to reset your password'
 
-#########################################################################
-## If you need to use OpenID, Facebook, MySpace, Twitter, Linkedin, etc.
-## register with janrain.com, uncomment and customize following
-# from gluon.contrib.login_methods.rpx_account import RPXAccount
-# auth.settings.actions_disabled=['register','change_password','request_reset_password']
-# auth.settings.login_form = RPXAccount(request, api_key='...',domain='...',
-#    url = "http://localhost:8000/%s/default/user/login" % request.application)
-## other login methods are in gluon/contrib/login_methods
-#########################################################################
 
 crud.settings.auth = None                      # =auth to enforce authorization on crud
 
 #########################################################################
-## Define your tables below (or better in another model file) for example
-##
-## >>> db.define_table('mytable',Field('myfield','string'))
-##
-## Fields can be 'string','text','password','integer','double','boolean'
-##       'date','time','datetime','blob','upload', 'reference TABLENAME'
-## There is an implicit 'id integer autoincrement' field
-## Consult manual for more options, validators, etc.
-##
-## More API examples for controllers:
-##
-## >>> db.mytable.insert(myfield='value')
-## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
-## >>> for row in rows: print row.id, row.myfield
+## Tables
 #########################################################################
 
 db.define_table('person',
@@ -112,6 +95,9 @@ db.define_table('item_log',
     Field('msg')
 )
 
+#########################################################################
+## Auto Complete Widgets
+#########################################################################
 db.item.Name.widget = SQLFORM.widgets.autocomplete(request,
                             db.item.Name, limitby=(0,40), min_length=0, orderby=db.item.Name)
 db.item.Category.widget = SQLFORM.widgets.autocomplete(request,
@@ -126,10 +112,10 @@ db.item.Status.widget = SQLFORM.widgets.autocomplete(request,
 db.item.CheckedOut.widget = SQLFORM.widgets.autocomplete(request, db.person.search_name,
                             id_field=db.person.id, limitby=(0,10), min_length=0, orderby=db.person.search_name)
 
-#from gluon.contrib.populate import populate
-#populate(db.person,50)
-#populate(db.item,200)
-#populate(db.item_log,500)
+
+#########################################################################
+## Setup Auth Groups
+#########################################################################
 
 if db(db.auth_group.role=="admin").count() == 0:
     auth.add_group(role = 'admin')
