b70662c127b7ef028bb8c3d22f00516fd3b3caae
jlew <jlew.blackout@gmail.com>
2011-03-02 15:18:31 -0500

    Added filter option and rss option to reports/itemsBy
    Now you can view rss by
    http://127.0.0.1:8000/inventory/report/itemsBy.rss/Category
    
    And filter the report by
    http://127.0.0.1:8000/inventory/report/itemsBy/Category?filter=Category Filter Here
    
    They may both be mixed as well.

--- controllers/report.py
+++ controllers/report.py
@@ -26,9 +26,50 @@ def itemsBy():
     out = {}
     for item in db(db.item.id > 0).select():
         the_key = item[request.args(0)]
-        
+
+        # Allow an item to be filtered (useful for rss)
+        if request.vars.filter and the_key != request.vars.filter:
+            continue
+
         if not out.has_key(the_key):
             out[the_key] = [item]
         else:
             out[the_key] += [item]
-    return dict(out=out)
+
+    # If rss, pad data
+    if request.extension == "rss":
+        entries = []
+        for key,lst in out.items():
+            checkedout = 0
+            desc = ""
+            for x in lst:
+                if x.CheckedOut:
+                    checkedout+=1
+
+                desc += itemToTableRow(x).xml()
+
+            entry = dict(
+                    title=T("%(category)s: %(out)d of %(total)d on Loan") % {
+                            "category": key,
+                            "out": checkedout,
+                            "total": len(out[key])
+                        },
+                    link="http://%s%s" % \
+                            (request.env.http_host, 
+                            URL('report','itemsBy', args=request.args(0), vars={'filter': key}, extension="")
+                            ),
+                    description=itemTableHeader().xml() + desc + "</table>"
+                )
+            entries.append(entry)
+
+        return dict(
+                    title=T("Inventory Report Feed").xml(),
+                    link="http://%s%s" % \
+                            (request.env.http_host, 
+                            URL('report','itemsBy', args=request.args(0), extension="")
+                     ),
+                    description="Inventory Report by " + request.args(0),
+                    entries=entries)
+    
+    else:
+        return dict(out=out)
